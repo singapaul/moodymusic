28195667b246d7d968445b4bd09dafb50d282d34
Paul <paulmichaelhardman@gmail.com>
2024-03-31 12:19:04 +0100

    wuu

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from dotenv import load_dotenv
+import openai
+import json
 from completionCall import call_playlist_prompt
 import os #provides ways to access the Operating System and allows us to read the environment variables
 
@@ -37,7 +39,41 @@ class Sum(Resource):
   
 class Moody(Resource):
     def post(self):
-        return call_playlist_prompt()
+        data=request.get_json()
+        count = data.get('count')
+        prompt = data.get('prompt')
+        example_json = """
+            [
+            {"song": "Sexyback", "artist": "Justin Timberlake"},
+            {"song": "Pony", "artist": "Ginuwine"},
+            {"song": "Talk Dirty", "artist": "Jason Derulo ft. 2 Chainz"},
+            {"song": "I'm a Slave 4 U", "artist": "Britney Spears"},
+            {"song": "Partition", "artist": "Beyoncé"},
+            {"song": "Earned It", "artist": "The Weeknd"},
+            {"song": "Buttons", "artist": "The Pussycat Dolls"},
+            {"song": "Motivation", "artist": "Kelly Rowland"},
+            {"song": "Slow Motion", "artist": "Trey Songz"},
+            {"song": "Pillowtalk", "artist": "Zayn"}
+            ]
+        """
+
+        messages = [
+        {"role" : "system", "content" : """You are a helpful playlist generating assistant. You should generate a list of songs and their artists according to a text prompt. You should return a JSON array where each element follows this format: {"song" : <song_title>, "artist" : <artist_name>}"""},
+        {"role": "user", "content" : "Generate a list 10 of songs based on this prompt:  love songs"}, 
+        {"role" : "assistant", "content" : example_json},
+        {"role": "user", "content" : f"Generate a list of {count} songs based on this prompt: {prompt}"}, 
+        ]
+
+
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=400,
+        )
+
+        playlist = json.loads(completion.choices[0].message.content)
+        return jsonify(playlist)
+
 
 
 class PostExample(Resource):
