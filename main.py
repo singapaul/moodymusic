7503a794b9bd9242a725d8f7227dd604db886be6
Paul <paulmichaelhardman@gmail.com>
2024-03-31 10:43:13 +0100

    update for production?

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os #provides ways to access the Operating System and allows us to read the environment variables
 
-load_dotenv()
+# load_dotenv()
 
 my_id = os.getenv("ID")
 my_secret_key = os.getenv("SECRET_KEY")
